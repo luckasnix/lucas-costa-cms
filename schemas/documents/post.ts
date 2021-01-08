222b469c8f77909b046e78e064ace22411dc99a8
Lucas Alves Costa <luc.kasnix@gmail.com>
2021-01-07 21:32:47 -0300

    Add post document and title field

---
+++ schemas/documents/post.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'post',
+  title: 'Postagem',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Título',
+      type: 'string'
+    }
+  ]
+}
--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,7 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import postDocument from './documents/post'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([
+    postDocument
+  ]),
 })
